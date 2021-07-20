9b9d1907d0be75a80bce73fb0cf2f4a9248a8016
Chiro <sachinsinghroxx@gmail.com>
2021-07-20 19:13:16 +0530

    adding some extra stuff

--- tripleX/TripleX.cpp
+++ tripleX/TripleX.cpp
@@ -2,22 +2,37 @@
 
 int main()
 {
+    // print the welcome message to the terminal
     std::cout << "You are a secret agent breaking into a secure server room...";
     std::cout << std::endl;
     std::cout << "Enter the correct code to continue...";
+    std::cout << std::endl;
 
-    int a = 4;
-    int b = 10;
-    int c = 2;
+    // declare tthe 3 number code
+    int CodeA = 4;
+    int CodeB = 10;
+    int CodeC = 2;
 
-    a = 10;
+    // find CodeSum and CodeProduct
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
-    const int x = a + b + c;
+    std::cout << " There are three numbers in the code \n";
+    std::cout << " The code add upto " << CodeSum << std::endl;
+    std::cout << " The code multiplCodeProduct upto " << CodeProduct << std::endl;
 
-    const int y = a*b*c;
+    int PlayerGuessA, PlayerGuessB, PlayerGuessC;
+    std::cin>>PlayerGuessA>>PlayerGuessB>>PlayerGuessC;
 
+    int GuessSum = PlayerGuessA + PlayerGuessB + PlayerGuessC;
+    int GuessProduct = PlayerGuessA * PlayerGuessB * PlayerGuessC;
 
-    std::cout << x<<" "<<y;
+    if (GuessSum == CodeSum && GuessProduct == CodeProduct){
+        std::cout << "You are a true agent!";
+    }else{
+        std::cout << "You are a false agent!";
+    }
+    // std::cout << "Player Guess: " << PlayerGuessA << " " << PlayerGuessB << " " << PlayerGuessC << std::endl;
 
     return 0;
 }
