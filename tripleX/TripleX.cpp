cc8c45ebc6609504f5f533f7607d409961cad602
Chiro <sachinsinghroxx@gmail.com>
2021-07-20 18:55:59 +0530

    varibales

--- tripleX/TripleX.cpp
+++ tripleX/TripleX.cpp
@@ -5,5 +5,16 @@ int main()
     std::cout << "You are a secret agent breaking into a secure server room...";
     std::cout << std::endl;
     std::cout << "Enter the correct code to continue...";
+
+    int a = 4;
+    int b = 10;
+    int c = 2;
+
+    int x = a + b + c;
+
+    int y = a*b*c;
+
+    std::cout << x<<" "<<y;
+
     return 0;
 }
